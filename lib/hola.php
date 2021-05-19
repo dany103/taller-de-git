2fdfe22ef67b26a533d580d9a3844fcdacfb5d55
Daniel <johndoe@example.com>
2021-05-19 16:22:36 +0200

    Solucionado el conflicto al fusionar con la rama master

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,13 @@
 <?php
 // Autor: Sergio Gómez <sergio@uco.es>
+<<<<<<< HEAD
 // El nombre por defecto es Mundo
+require('HolaMundo.php');
+
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+print new HolaMundo($nombre);
+=======
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
+>>>>>>> master
